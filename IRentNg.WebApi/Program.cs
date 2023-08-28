c37ef7a01658469cf94dd07311629ed5434e28fd
Dee-Joshua <officialdariajoshua@gmail.com>
2023-08-28 17:57:27 +0100

    Project Configuration

--- IRentNg.WebApi/Program.cs
+++ IRentNg.WebApi/Program.cs
@@ -1,6 +1,9 @@
+using IRentNG.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
